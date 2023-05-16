1db34e84b68db8a44ae3774c2fb128c550f09713
Jamie Morrissey <jamie_morrissey@Jamies-MacBook-Air-2.local>
2023-05-15 21:50:31 -0700

    added two files to source

---
+++ file.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+        throw 'a';
+    }
+    
+    catch (int x) {
+        cout << "Caught" << x;
+    }
+    catch (...) {
+        cout << "Default Exception\n";
+    }
+    return 0;
+}
